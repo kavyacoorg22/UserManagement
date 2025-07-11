3fe10a64e0b4d134589cbe6a1c3b64fa82e0bc32
Kavya <kavyakopatti99@gmail.com>
2025-07-11 11:04:43 +0530

    custom thunk

---
+++ frontend/utils/userThunks.jsx
@@ -0,0 +1,50 @@
+import { replace } from "react-router-dom";
+import { setCurrentUser} from "./userSlice";
+import { toast } from "react-toastify";
+
+export const loginUser = (email, password, navigate) => async (dispatch) => {
+  try {
+    const res = await fetch("/user/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+       credentials: 'include',
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      toast.success("Login Successful ");
+      dispatch(setCurrentUser(data.user));
+     
+      navigate("/home",{replace:true});
+    } else {
+      toast.error(data.message || "Login Failed ");
+    }
+  } catch (err) {
+    toast.error("Network Error");
+  }
+};
+
+export const registerUser = (formData, setLogin, setErrorMessage) => async (dispatch) => {
+  try {
+    const res = await fetch("/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      toast.success("Registered Successfully");
+      setLogin(true);
+    } else {
+      setErrorMessage(data.message || "Register Failed");
+      toast.error(data.message || "Register Failed");
+    }
+  } catch (err) {
+    setErrorMessage("Network error");
+    toast.error("Network error");
+  }
+};
